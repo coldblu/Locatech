763db2aae75b93f0ec84e92b14b2d76db7ed36fc
Yuri <yuri.230@gmail.com>
2022-07-18 09:47:20 -0300

    aplicado estilos
    implementado a folha de estilo nas páginas

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
 	<meta name="author" content=""/>
 	<meta name="description" content="Locatech - Sistema de locação de dispositivos IOS."/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/> <!--Meta tag responsiva -->
-	<!--<link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen"/> -->
+	<link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen"/>
 </head>
 <body>
 	<div >
